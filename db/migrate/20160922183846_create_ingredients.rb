8b6fea76c5488cfcfc164597aef263d62e722b1b
nlkassad <nlkassad@hotmail.com>
2016-09-22 14:47:08 -0400

    Add Controller, Model and db Migration
    Add controller with model for ingredients and migrate the
    ingredients with updated field.

---
+++ db/migrate/20160922183846_create_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateIngredients < ActiveRecord::Migration
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.string :std_measure
+      t.string :color
+      t.string :toxicity
+
+      t.timestamps null: false
+    end
+  end
+end
