e3311726a8acbfd0ecbcdc8c2c12211966dbe69b
nlkassad <nlkassad@hotmail.com>
2016-09-22 18:08:26 -0400

    Add Route for Ingredient
    Add show route for ingredient to match the GET call to
    ingredient with id.

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 
   resources :ingredients, except: [:new, :edit]
   get '/ingredients', to: 'ingredients#index'
+  get '/ingredient/:id', to: 'ingredient#index'
+
 end
